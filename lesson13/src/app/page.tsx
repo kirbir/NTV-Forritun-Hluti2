ef4229fe1d8e05fc3afb94b3be3dff28bc771b61
Birkir Reynisson <109977046+kirbir@users.noreply.github.com>
2025-03-28 08:01:57 +0000

    work on React CV

--- lesson13/src/app/page.tsx
+++ lesson13/src/app/page.tsx
@@ -1,25 +1,65 @@
 "use client";
 import Image from "next/image";
-import {Sections} from "../data/sections"
-
+import { Sections } from "../data/sections";
 
 const Home = () => {
-  const foo = () => <p>hi</p>;
   return (
-    <div className="w-full-lg mx-auto px-10">
+    <div className="max-w-screen-lg mx-auto px-10 pt-10">
       <header className="flex flex-col justify-center p-6 items-center rounded-t-3xl bg-blue-500">
+        <Image
+          className="rounded-full mb-4 "
+          src="/shrek.jpg"
+          width="100"
+          height="100"
+          alt=""
+        />
         <h1 className="text-3xl">{Sections[0].header.title}</h1>
         <h2 className="text-xl">{Sections[0].header.subTitle}</h2>
       </header>
-      <section className="flex flex-row p-6">
-      <h3 className="text-3xl">Phone: {Sections[1].contact.phone}</h3>
-      <h3 className="text-3xl">E-mail: {Sections[1].contact.email}</h3>
-      <h3 className="text-3xl">Website: {Sections[1].contact.web}</h3>
-    
+
+      <section className="flex flex-row justify-between text-sm p-6 mx-10 flex-nowrap gap-4">
+        <h3 className="">Phone: {Sections[1].contact.phone}</h3>
+        <h3 className="">E-mail: {Sections[1].contact.email}</h3>
+        <h3 className="">Website: {Sections[1].contact.web}</h3>
+      </section>
+
+      <section className="flex flex-col p-6 bg-blue-500">
+        <h2 className="font-extrabold">Um mig</h2>
+        <p>{Sections[2].about.text}</p>
+      </section>
+
+      <section className="flex flex-col gap-4 p-6 bg-blue-400">
+        <ul className="flex gap-5">
+          {Sections[3].reynsla.companies.map((company, index) => (
+            <li key={index}>
+              <h2 className="text-lg font-bold">{company.companyName}<span> - </span><span className="font-extralight">{company.year}</span></h2>
+              <h4>{company.jobTitle}</h4>
+              <p className="text-base text-blue-950">{company.description}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className="flex flex-row justify-center gap-4 p-6 bg-blue-300">
+        <ul className="flex gap-5">
+          {Sections[4].menntun.map((school,index) => (
+            <li key={index}>
+            <h2 className="text-lg font-bold">{school.namsbraut}</h2><span> - </span><span>{school.year}</span>
+            <p>{school.skoli}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
+      {/* Skill Chips*/}
+      <section className="flex flex-row p-10 gap-4">
+         {Sections[5].skills.map((d,index) => (
+         <p key={index}className="inline-block px-2 py-1 border-1 border-amber-100">
+          {d.skillName}
+          </p>
+        ))}
       </section>
     </div>
-  )
+  );
 };
-
 
 export default Home;
--- lesson13/src/data/sections.ts
+++ lesson13/src/data/sections.ts
@@ -1,20 +1,79 @@
-
-
-export const Sections =
-    [
+export const Sections = [
+  {
+    header: {
+      title: "Birkir Viðar Reynisson",
+      subTitle: "Forritari",
+    },
+  },
+  {
+    contact: {
+      email: "birkirvr@gmail.com",
+      phone: "790-8585",
+      web: "github.com/kirbir",
+    },
+  },
+  {
+    about: {
+      text: "Í dag er ég að bæta við mig forritunarþekkingu á annarri önn af þremmur á forritunarbraut hjá NTV. Gervigreind, veflausnir, öpp og tölvuleikir þar er framtíðin mín.",
+      title: "Um mig",
+    },
+  },
+  {
+    reynsla: {
+      companies: [
         {
-            header: {
-                title: 'Birkir Viðar Reynisson',
-                subTitle: 'Forritari'
-            },
-
+          companyName: "Ayurveda Ehf",
+          description:
+            "Hef verið að aðstoða í hlutastarfi að byggja upp netverslun Pureshilajit.is með góðum árangri. Hannaði síðuna og viðheld henni og starfa að markaðsetningu og grafískri hönnun og vefforritun fyrir Ayurveda ehf.",
+          jobTitle: "Vefhönnuður, Markaðssetning",
+          year: "2024-2025",
         },
         {
-        contact: {
-            email: 'Birkir Viðar Reynisson',
-            phone: 'Forritari',
-            web: 'github.com/kirbir'
+          companyName: "Silfurfat",
+          description:
+            "Hef sinnt alhliða tækniþjónustu fyrir fyrirtæki. Meðal annars: Samskipti ehf, Sýningarkerfi. Sinnti þjónustu með hugbúnað, útstöðvar, netþjóna, síma og prentara svo einhvað sé nefnt.",
+          jobTitle: "Kerfistjóri, Tækniþjónusta",
+          year: "2013-2021",
         },
+        {
+          companyName: "Tölvustoð Ehf",
+          description:
+            "Starfaði sem tæknimaður, vann við þróun og ráðgjöf á tæknilausnum fyrir fjölda fyrirtækja í útköllum og rekstur á hýsingarlausnum og netkerfum.",
+          jobTitle: "Kerfistjóri, Tækniþjónusta",
+          year: "2007-2013",
+        },
+      ],
+    },
+  },
+  {
+    menntun: [
+        {
+          skoli: "NTV",
+          namsbraut: "Forritunarbraut Diplómanám",
+          year:"2024-2025",
+        },
+        {
+          skoli: "NTV",
+          namsbraut: "Microsoft MCSA/MCP & Comptia A++",
+          year:"2006-2007",
+        },
+      ],
+  },
+  {
+    skills: [
+        {
+          skillName: "Frumkvæði",
+        },
+        {
+          skillName: "Vinalegur"
+        },
+        {
+            skillName:"Vinna í teymi"
+        },
+        {
+            skillName:"Húmor"
         }
-
-    ] as const;
+      ],
+  }
+  
+] as const;
